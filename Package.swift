99d67fa4a09cce71a130458bb88ab69a7c0300c1
Patric Duboids <patric.dubois@apijockey.com>
2025-12-29 16:22:12 +0100

    updated license info in package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,16 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// This source file is part of the Swift open source project
+//
+// Copyright (c) 2025 CgSe Computergrafik und Softwareentwicklung GmbH and the Swift project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import PackageDescription
 
 let package = Package(
