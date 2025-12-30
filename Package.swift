ffa8110c5ebe64180e6c617e650a9b693f182c05
Patric Duboids <patric.dubois@apijockey.com>
2025-12-30 14:47:31 +0100

    tools version back to line 1 and cleaned up releases

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version: 5.9
 /*
  * Copyright 2025 CgSe Computergrafik und Softwareentwicklung GmbH
  *
@@ -13,33 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-/*
- * Copyright 2025 
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
-// Copyright (c) 2025 CgSe Computergrafik und Softwareentwicklung GmbH
-// Licensed under Apache License v2.0
-//
-// See LICENSE.txt for license information
-//
-// SPDX-License-Identifier: Apache-2.0
-
 import PackageDescription
 
 let package = Package(
